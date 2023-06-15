67a3bd69520e0042d80542781b305b663306ae28
Himanshu MacTavish <Himanshu.mactavish@pm.me>
2023-06-15 12:20:44 -0400

    Added custom echo

---
+++ Chapter-05/functions.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	FirstName string
+	lastName  string
+	Age       int
+}
+
+func main2() {
+	people := []Person{
+		{"pat", "Patterson", 17},
+		{"Tracey", "Bobbert", 23},
+		{"Fred", "Fredson", 32},
+	}
+	for i := 0; i < 5; i++ {
+		func(j int) {
+			fmt.Println("Anonymus Function: ", j)
+		}(i)
+	}
+	fmt.Println(people)
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+	fmt.Println(people)
+}
